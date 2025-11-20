87893233cb7142786aa9188e120ff3a701fd2060
Sahiljeet Singh <122285024+Sahiljeet1021@users.noreply.github.com>
2025-11-20 17:38:11 +0530

    Update input prompt to reflect ATS perspective

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ with col_b:
 
 # Prompts
 input_prompt1 = """
-You are an experienced Technical Human Resource Manager. Your task is to review the provided resume against the job description. 
+You are an experienced ATS (Applicant Tracking System). Your task is to review the provided resume against the job description. 
 Please share your professional evaluation on whether the candidate's profile aligns with the role. 
 Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
